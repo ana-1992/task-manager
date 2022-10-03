3621557ed86f78864b335167c31a8babf8ddcaf6
Anastasia <ana.zhu1992@gmail.com>
2022-10-03 14:38:48 +0300

    update sequence

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :name, :description, :state] do |n|
     "string#{n}"
+  end
+  sequence :email do |n|
+    "person#{n}@gmail.com"
+  end
+  sequence :expired_at do |n|
+    Date.today
   end
 end
