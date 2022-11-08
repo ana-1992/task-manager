4e985af29420c1b5bdc64a782629c1ec1a988a8c
Anastasia <ana.zhu1992@gmail.com>
2022-11-08 22:23:54 +0300

    refactor: set correct hierarchy

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,4 @@
-class Admin::UsersController < ApplicationController
-  include AuthHelper
-  before_action :authenticate_user!, :authorize
-  helper_method :current_user
-
+class Admin::UsersController < Admin::ApplicationController
   def show
     @user = User.find(params[:id])
   end
@@ -11,7 +7,7 @@ class Admin::UsersController < ApplicationController
     @q = User.ransack(params[:q])
     @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
@@ -20,38 +16,30 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
-  def authorize
-    render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false) if forbidden?
-  end
-
-  def forbidden?
-    !current_user.is_a?(Admin)
-  end
-
   def create
     @user = User.new(user_params)
-     if @user.save
-      redirect_to admin_users_url
+    if @user.save
+      redirect_to(admin_users_url)
     else
-      render :new_admin_user_url
+      render(:new_admin_user_url)
     end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to admin_users_url
+      redirect_to(admin_users_url)
     else
-      render :edit
+      render(:edit)
     end
   end
-  
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_path
+    redirect_to(admin_users_path)
   end
-  
+
   private
 
   def user_params
