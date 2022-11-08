05ebe2df689b809054dbe487d4734433f3e2d495
Anastasia <ana.zhu1992@gmail.com>
2022-11-08 14:17:20 +0300

    fix linter issues

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,18 +1,16 @@
 class Admin::UsersController < ApplicationController
-    include AuthHelper
-    before_action :authenticate_user!, :authorize
+  include AuthHelper
+  before_action :authenticate_user!, :authorize
 
-    def show
-        @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def authorize
-      render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false) if (forbidden?)
-    end
+  def authorize
+    render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false) if forbidden?
+  end
 
-    def forbidden?
-      !current_user.is_a?(Admin)
-    end
-      
+  def forbidden?
+    !current_user.is_a?(Admin)
+  end
 end
-
